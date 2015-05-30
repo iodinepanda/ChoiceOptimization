b31e70c5c661442e6c0e15da379aabc85f1bc759
mmussomele <mmussomele@berkeley.edu>
2015-05-30 17:08:22 -0400

    refined .travis.yml, set up ant builds for automatic compilation and test execution

---
+++ testing/TestLauncher.java
@@ -0,0 +1,38 @@
+package testing;
+
+/**
+ * Copyright (C) 2015 Matthew Mussomele
+ *
+ *  This file is part of ChoiceOptimizationAlgorithm
+ *  
+ *  ChoiceOptimizationAlgorithm is free software: you can redistribute it 
+ *  and/or modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of the 
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestLauncher {
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestSuite.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println(String.format("Ran %d tests.\n%d failed.\n%d succeeded.", 
+                                    result.getRunCount(), result.getFailureCount(), 
+                                    result.getRunCount() - result.getFailureCount()));
+    }
+
+}
